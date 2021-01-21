e4d7c3fed3fe29df7688d9c8836d73e173d945f1
Murat <Murat@DESKTOP-QDM0RD8>
2021-01-21 19:46:42 +0300

    add DictonaryIntro

---
+++ DictonaryIntro/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DictonaryIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictonary<int, string> customers = new MyDictonary<int, string>();
+
+            customers.Add(1, "Murat");
+            customers.Add(2, "Kenan");
+            customers.Add(3, "Adem");
+
+        }
+    }
+}
